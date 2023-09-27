452a83c49b8efc0033053f0c3a5cfc9c4b08c1d7
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2023-09-27 15:21:44 -0300

    feat(wordcloud): wordlcoud

--- src/pages/resultados.tsx
+++ src/pages/resultados.tsx
@@ -1,30 +1,42 @@
 import { Footer } from "@/components/Footer";
 import { Header } from "@/components/Header";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
-import Chart from "chart.js/auto";
-import ChartDataLabels from "chartjs-plugin-datalabels";
 import { questionsEnum } from "@/constants/questionsEnum";
+import ReactWordcloud from "react-wordcloud";
 
 const Results = () => {
   const [product01Data, setProduct01Data] = useState([]);
   const [product02Data, setProduct02Data] = useState([]);
-  const chartRef01 = useRef(null);
-  const chartRef02 = useRef(null);
+
+  const options = {
+    rotations: [1, 1],
+    rotationAngles: [0, 0],
+    fontFamily: "sans-serif",
+    fontSizes: [16, 36],
+  };
 
   useEffect(() => {
     axios
       .get("/api/answers")
       .then((response) => {
         setProduct01Data(
-          response.data.filter(
-            (item: any) => item.product === 1 && item.quantity > 0
-          )
+          response.data
+            .filter((item: any) => item.product === 1 && item.quantity > 0)
+            .map((item: any) => ({
+              //@ts-ignore
+              text: questionsEnum[item.key],
+              value: item.quantity,
+            }))
         );
         setProduct02Data(
-          response.data.filter(
-            (item: any) => item.product === 2 && item.quantity > 0
-          )
+          response.data
+            .filter((item: any) => item.product === 2 && item.quantity > 0)
+            .map((item: any) => ({
+              //@ts-ignore
+              text: questionsEnum[item.key],
+              value: item.quantity,
+            }))
         );
       })
       .catch((error) => {
@@ -32,95 +44,7 @@ const Results = () => {
       });
   }, []);
 
-  useEffect(() => {
-    if (product01Data.length) {
-      //@ts-ignore
-      const ctx = chartRef01?.current?.getContext("2d");
-
-      new Chart(ctx, {
-        type: "pie",
-        data: {
-          //@ts-ignore
-          labels: product01Data.map((item) => questionsEnum[item.key]),
-          datasets: [
-            {
-              //@ts-ignore
-              data: product01Data.map((item) => item.quantity),
-              backgroundColor: [
-                "#1f36c7",
-                "#39b6e3",
-                "#9e3db4",
-                "#9b2c42",
-                "#257633",
-                "#666820",
-                "#3f6b26",
-                "#652323",
-                "#1c4048",
-                "#391630",
-              ],
-            },
-          ],
-        },
-        plugins: [ChartDataLabels],
-        options: {
-          plugins: {
-            datalabels: {
-              color: "white",
-              font: {
-                weight: "bold",
-                size: 18,
-              },
-            },
-          },
-        },
-      });
-    }
-  }, [product01Data]);
-
-  useEffect(() => {
-    if (product02Data.length) {
-      //@ts-ignore
-      const ctx = chartRef02?.current?.getContext("2d");
-
-      new Chart(ctx, {
-        type: "pie",
-        data: {
-          //@ts-ignore
-          labels: product02Data.map((item) => questionsEnum[item.key]),
-          datasets: [
-            {
-              //@ts-ignore
-              data: product02Data.map((item) => item.quantity),
-              backgroundColor: [
-                "#1f36c7",
-                "#39b6e3",
-                "#9e3db4",
-                "#9b2c42",
-                "#257633",
-                "#666820",
-                "#3f6b26",
-                "#652323",
-                "#1c4048",
-                "#391630",
-              ],
-            },
-          ],
-        },
-        plugins: [ChartDataLabels],
-        options: {
-          plugins: {
-            datalabels: {
-              color: "white",
-              font: {
-                weight: "bold",
-                size: 18,
-              },
-            },
-          },
-        },
-      });
-    }
-  }, [product01Data]);
+  console.log(product01Data);
 
   return (
     <>
@@ -133,19 +57,27 @@ const Results = () => {
           {product01Data.length > 0 && (
             <div className="flex flex-col justify-center">
               <p className="text-center mb-[16px]">Omo Litro</p>
-              <canvas
-                className="max-w-[400px] max-h-[400px] min-w-[400px] min-h-[400px]"
-                ref={chartRef01}
-              />
+              <div className="max-w-[400px] max-h-[400px] min-w-[400px] min-h-[400px]">
+                <ReactWordcloud
+                  size={[400, 400]}
+                  words={product01Data}
+                  //@ts-ignore
+                  options={options}
+                />
+              </div>
             </div>
           )}
           {product02Data.length > 0 && (
             <div className="flex flex-col justify-center">
               <p className="text-center mb-[16px]">Omo Saco</p>
-              <canvas
-                className="max-w-[400px] max-h-[400px] min-w-[400px] min-h-[400px]"
-                ref={chartRef02}
-              />
+              <div className="max-w-[400px] max-h-[400px] min-w-[400px] min-h-[400px]">
+                <ReactWordcloud
+                  size={[400, 400]}
+                  words={product02Data}
+                  //@ts-ignore
+                  options={options}
+                />
+              </div>
             </div>
           )}
         </div>
