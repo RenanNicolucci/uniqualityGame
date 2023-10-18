8d8676b6ea6ad9e3fb131309eb70e894902c5eb8
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2023-10-18 15:04:40 -0300

    feat(ranking screen): ranking screen

--- src/pages/[productID]/[userID]/gabarito.tsx
+++ src/pages/[productID]/[userID]/gabarito.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import type { GetServerSidePropsContext } from 'next';
+import { useRouter } from 'next/router';
 import React from 'react';
 
 import { Footer } from '@/components/Footer';
@@ -15,12 +16,14 @@ const Gabarito = ({
     user: { name },
   },
 }: any) => {
+  const route = useRouter();
+  const { productID } = route.query;
   return (
     <>
       <Header />
       <main>
         <div className="m-auto flex w-full flex-col items-center justify-center gap-[32px] p-[32px] md:flex-row md:items-center md:justify-center md:gap-[64px]">
-          <div>
+          <div className="min-w-[350px]">
             <h1 className="text-center text-[20px]">{`${name} seu gabarito:`}</h1>
             <div className="mt-[32px] flex flex-col gap-[16px]">
               <div className="flex w-full items-center justify-between">
@@ -43,11 +46,55 @@ const Gabarito = ({
                       answerValue: typeof QuestionsEnum;
                       id: number;
                     }) => (
-                      // @ts-ignore
-                      <p key={answer.id}>{QuestionsEnum[answer.answerValue]}</p>
+                      <div
+                        key={answer.id}
+                        className="flex items-center justify-between"
+                      >
+                        {/* @ts-ignore */}
+                        <p>{QuestionsEnum[answer.answerValue]}</p>
+                        <p className="text-[#076607]">verdadeiro</p>
+                      </div>
                     ),
                   )}
                 </div>
+              </div>
+              {wrongAnswers.length ? (
+                <div className="mt-[32px]">
+                  <p>Suas respostas incorretas:</p>
+                  <div className="mt-[16px] flex flex-col gap-[16px]">
+                    {wrongAnswers.map(
+                      (answer: {
+                        key: typeof QuestionsEnum;
+                        id: number;
+                        value: boolean;
+                      }) => (
+                        <div
+                          key={answer.id}
+                          className="flex items-center justify-between"
+                        >
+                          {/* @ts-ignore */}
+                          <p>{QuestionsEnum[answer.key]}</p>
+                          {answer.value ? (
+                            <p className="text-[#076607]">verdadeiro</p>
+                          ) : (
+                            <p className="text-[#d10f09]">falso</p>
+                          )}
+                        </div>
+                      ),
+                    )}
+                  </div>
+                </div>
+              ) : null}
+              <div>
+                <button
+                  type="button"
+                  onClick={() => {
+                    route.push(`/${productID}/ranking`);
+                  }}
+                  className="mt-[22px] flex w-full items-center justify-center gap-[16px] rounded bg-[#1f36c7] p-[8px] px-[22px] font-bold uppercase text-white"
+                >
+                  Visualizar Ranking
+                </button>
               </div>
             </div>
           </div>
---
+++ src/pages/[productID]/ranking.tsx
@@ -0,0 +1,70 @@
+import axios from 'axios';
+import type { GetServerSidePropsContext } from 'next';
+import React from 'react';
+
+import { Footer } from '@/components/Footer';
+import { Header } from '@/components/Header';
+
+const Ranking = (data: any) => {
+  return (
+    <>
+      <Header />
+      <main>
+        <div className="m-auto flex w-full flex-col items-center justify-center gap-[32px] p-[32px] md:flex-row md:items-center md:justify-center md:gap-[64px]">
+          <div className="min-w-[350px]">
+            <h1 className="text-center text-[20px]">Ranking</h1>
+            <div className="mt-[32px] flex flex-col gap-[16px]">
+              <table>
+                <tr>
+                  <th>Nome</th>
+                  <th>Acertos</th>
+                  <th>Posição</th>
+                </tr>
+                {data.data.map((item: any, index: number) => (
+                  <tr key={item.user.id}>
+                    <td className="p-[8px]">{item.user.name}</td>
+                    <td className="p-[8px] text-right">
+                      {item.correctAnswers.length || 0}/3
+                    </td>
+                    <td className="p-[8px] text-right">{`${index + 1}º`}</td>
+                  </tr>
+                ))}
+              </table>
+              <div>
+                <button
+                  type="button"
+                  className="mt-[22px] flex w-full items-center justify-center gap-[16px] rounded bg-[#1f36c7] p-[8px] px-[22px] font-bold uppercase text-white"
+                >
+                  Visualizar Palavras
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>
+      <Footer />
+    </>
+  );
+};
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const { productID } = context.query;
+
+  try {
+    const response = await axios.get(
+      `http://localhost:3000/api/ranking/${productID}`,
+    );
+
+    return {
+      props: {
+        data: response.data.sortByRanking,
+      },
+    };
+  } catch (error) {
+    return {
+      notFound: true,
+    };
+  }
+}
+
+export default Ranking;
