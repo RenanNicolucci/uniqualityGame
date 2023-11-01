0bb690c6a8a262d08d11ae84d73f24241a06165b
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2023-11-01 09:09:33 -0300

    fix(mobile): mobile header fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -13,19 +13,19 @@ export const Header = () => {
   return (
     <header>
       <div className="flex items-center justify-between  gap-[16px] bg-[#1f36c7]">
-        <div className="flex w-full items-center gap-[32px]">
+        <div className="flex w-full items-center gap-[16px] md:gap-[32px]">
           <div className="bg-[#4056e6] p-[20px] md:w-[130px]">
             <div className="relative h-[45px] w-[45px] md:h-[90px] md:w-[90px]">
               <Image src="/assets/logo.webp" alt="productImage" fill />
             </div>
           </div>
           <div>
-            <p className="text-[14px] text-[white] md:text-[22px]">
+            <p className=" text-[14px] text-[white] md:inline md:text-[22px]">
               UniQualityGame
             </p>
           </div>
         </div>
-        <div className="flex items-center pr-[60px] text-[white]">
+        <div className="flex items-center pr-[16px] text-[white] md:pr-[60px]">
           <select
             className="bg-[#141c51] px-[16px] py-[8px]"
             onChange={(e) => {
