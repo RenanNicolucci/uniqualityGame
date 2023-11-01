0f19339bde13861405a73c00b8e3e9bfe8ec885c
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2023-11-01 10:48:06 -0300

    feat(i18n): translate

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,44 @@
 import '@/styles/global.css';
 import 'react-toastify/dist/ReactToastify.css';
 
+import Backend from 'i18next-http-backend';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
-import { appWithTranslation } from 'next-i18next';
+import { appWithTranslation, i18n } from 'next-i18next';
+import { initReactI18next } from 'react-i18next';
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ToastContainer } from 'react-toastify';
 
+import eua from '../../public/locales/en-US/common.json';
+import spanish from '../../public/locales/es/common.json';
+import brasil from '../../public/locales/pt-BR/common.json';
+
 const queryClient = new QueryClient();
+
+const resources = {
+  'pt-BR': {
+    translation: brasil,
+  },
+  'en-US': {
+    translation: eua,
+  },
+  es: {
+    translation: spanish,
+  },
+};
+
+if (i18n) {
+  i18n
+    .use(Backend)
+    .use(initReactI18next)
+    .init({
+      resources,
+      fallbackLng: 'pt-BR', // Fallback language
+      interpolation: {
+        escapeValue: false, // not needed for React
+      },
+    });
+}
 
 const MyApp = ({ Component, pageProps }: AppProps) => (
   <>
