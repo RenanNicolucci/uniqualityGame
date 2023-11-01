96ac8bbdec4bdad1cd3e31269d412e016f243dfb
joaogabrielfragosojardim <joaogabrielmgtsu@gmail.com>
2023-10-31 22:38:44 -0300

    feat(responsive): mobile header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -10,15 +10,17 @@ export const Header = () => {
   };
   return (
     <header>
-      <div className="flex items-center  justify-between bg-[#1f36c7]">
+      <div className="flex items-center justify-between  gap-[16px] bg-[#1f36c7]">
         <div className="flex w-full items-center gap-[32px]">
-          <div className="w-[130px] bg-[#4056e6] p-[20px]">
-            <div className="relative h-[90px] w-[90px]">
+          <div className="bg-[#4056e6] p-[20px] md:w-[130px]">
+            <div className="relative h-[45px] w-[45px] md:h-[90px] md:w-[90px]">
               <Image src="/assets/logo.webp" alt="productImage" fill />
             </div>
           </div>
           <div>
-            <p className="text-[22px] text-[white]">UniQualityGame</p>
+            <p className="text-[14px] text-[white] md:text-[22px]">
+              UniQualityGame
+            </p>
           </div>
         </div>
         <div className="flex items-center pr-[60px] text-[white]">
